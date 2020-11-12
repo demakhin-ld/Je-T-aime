7fe1181d6f5952ae5dfaed0d33c4ce1355b733ba
litvinovl-digital <70130791+litvinovl-digital@users.noreply.github.com>
2020-11-12 22:31:14 +0300

    настрока js

--- src/js/main.js
+++ src/js/main.js
@@ -125,7 +125,11 @@ $(document).ready(function () {
 $(document).ready(function () {
   $('.header__burger').click(function (event) {
     $('.header__burger, .header__menu').toggleClass('active');
-    $('body').toggleClass('lock')
+    $('body').toggleClass('lock');
+  });
+  $('.header__link').click(function () {
+    $('.header__burger, .header__menu').removeClass('active');
+    $('body').removeClass('lock');
   });
 });
 
