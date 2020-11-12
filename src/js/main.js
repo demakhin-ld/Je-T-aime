f3c0f920cbc380a217041db7bdca7ceacf685286
Dmitry_DS <demakhin@l-digital.ru>
2020-11-12 23:02:09 +0300

    header scroll

--- src/js/main.js
+++ src/js/main.js
@@ -110,16 +110,17 @@ window.onload = function () {
       ElemTabs[b].classList.add('show');
     }
   }
+
+  $(document).ready(function () {
+    $("#menu").on("click", "a", function (event) {
+      event.preventDefault();
+      var id = $(this).attr('href'),
+        top = $(id).offset().top;
+      $('body,html').animate({ scrollTop: top }, 1500);
+    });
+  });
 }
 
-$(document).ready(function () {
-  $("#menu").on("click", "a", function (event) {
-    event.preventDefault();
-    var id = $(this).attr('href'),
-      top = $(id).offset().top;
-    $('body,html').animate({ scrollTop: top }, 1500);
-  });
-});
 
 
 $(document).ready(function () {
